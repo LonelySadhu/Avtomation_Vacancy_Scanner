32fdc30e276c10efeb746c4b16a6aac0db34a1fc
Andrey Novikov <novikovandrey1984@gmail.com>
2019-08-31 16:01:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from vac_parser import request_to_hh, input_params, page_parser
-from vacancy_db import create_csv_db, save_to_db, show_db
+from vacancy_db import show_db, clear_db, show_fresh
 
 
 if __name__ == '__main__':
     docs = request_to_hh(input_params())
     page_parser(docs)
     show_db()
+    show_fresh()
+    #clear_db()
